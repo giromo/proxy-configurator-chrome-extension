e2be640d1a894b13f2876c61f7b5cf176659529e
giromo <giromonti94@gmail.com>
2025-10-23 18:51:10 +0330

    Create icons.tsx

---
+++ src/components/icons.tsx
@@ -0,0 +1,13 @@
+import { LucideIcon } from "lucide-react";
+import { ChevronDown, Check, Loader2, Info, CircleCheck, CircleX, CircleDot, RefreshCw } from "lucide-react";
+
+export const Icons = {
+  chevronDown: ChevronDown,
+  check: Check,
+  loader2: Loader2,
+  info: Info,
+  circleCheck: CircleCheck,
+  circleX: CircleX,
+  circleDot: CircleDot,
+  refreshCw: RefreshCw,
+} as const;
